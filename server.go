17ee47aa02621d08bc1e3836980fd561e7c1f2f7
nicolas <nikolastesone@gmail.com>
2021-03-07 00:27:07 -0300

    first test of gin

---
+++ server.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/nictes1/Gin-Gonic--Golang/controller"
+	"github.com/nictes1/Gin-Gonic--Golang/service"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/posts", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
+	})
+
+	server.Run(":8080")
+
+}
