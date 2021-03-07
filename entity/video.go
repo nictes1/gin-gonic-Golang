aa3036665d5c29a37a4fe134a50398923deea621
nicolas <nikolastesone@gmail.com>
2021-03-07 14:34:39 -0300

    validations-gin

--- entity/video.go
+++ entity/video.go
@@ -1,7 +1,15 @@
 package entity
 
+type Person struct {
+	FirsName string `json: "firstname" binding: "requiered"`
+	LastName string `json: "lastname" binding: "requiered"`
+	Age      int8   `json: "age" binding: "gte=1, lte=130"`
+	Email    string `json: "email" validate: "requiered, email"`
+}
+
 type Video struct {
-	Title       string `json: "title"`
-	Description string `json: "description"`
-	URL         string `json: "url"`
+	Title       string `json: "title" binding: "min=2, max=10" validate:"is-cool"`
+	Description string `json: "description" binding: "max=20"`
+	URL         string `json: "url" binding: "required,url"`
+	Auth        string `json: "author" binding: "required"`
 }
